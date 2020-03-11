31773c5ec5dbcddfeb5ffa6c3b3cceb2b6b791a8
Marije <49620465+marijedb@users.noreply.github.com>
2020-03-12 00:32:11 +0100

    Updates

---
+++ precache-manifest.c38530e884b7d857759a4c93a55bd97d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44762c321f41bf6c919e9b06aadac33a",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "08678477e1bd52d14b55",
+    "url": "/starwars/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1b9a0b41a9a7a6c8a58b",
+    "url": "/starwars/static/css/main.b3cf979d.chunk.css"
+  },
+  {
+    "revision": "08678477e1bd52d14b55",
+    "url": "/starwars/static/js/2.4e26d722.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/starwars/static/js/2.4e26d722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9a0b41a9a7a6c8a58b",
+    "url": "/starwars/static/js/main.4bddaf0c.chunk.js"
+  },
+  {
+    "revision": "d72637765ed70a73063e",
+    "url": "/starwars/static/js/runtime-main.0a38ad60.js"
+  },
+  {
+    "revision": "8dc6d9a48fa9b9411f2316728a7dc43f",
+    "url": "/starwars/static/media/stars.8dc6d9a4.jpg"
+  }
+]);
