6169848d17c4b73cc0fea11ad32fbee554a963e8
Marije <49620465+marijedb@users.noreply.github.com>
2020-03-13 19:29:28 +0100

    updated Card Component

--- src/containers/Card.js
+++ src/containers/Card.js
@@ -19,14 +19,14 @@ class Card extends Component {
             <div className="tc f7-ns f7-m f4-l bg-near-black yellow dib pa3 ma2 grow shadow-5 h5 ba bw1 w-30-ns w-40-m w-100 b--yellow">
               <div>
                 <h2>{this.props.name}</h2>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
                 <button
                   className="f5 link dim ba bw1 b--black ph3 pv2 mb2 dib black w-100 h-50 bg-light-yellow"
                   onClick={this.flipCard}
                 >
                   Show information
                 </button>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
               </div>
             </div>
           );
@@ -35,14 +35,14 @@ class Card extends Component {
             <div className="tc f7-ns f7-m f4-l bg-near-black yellow dib pa3 ma2 grow shadow-5 h5 ba bw1 w-30-ns w-40-m w-100 b--yellow">
               <div>
                 <h2>{this.props.name}</h2>
+                <p>Birth year: {this.props.birth_year}</p>
+                <p>Hair color: {this.props.hair_color}</p>
                 <button
                   className="f5 link dim ba bw1 b--black ph3 pv2 mb2 dib black w-100 h-50 bg-light-yellow"
                   onClick={this.flipCard}
                 >
                   Hide information
                 </button>
-                <p>Birth year: {this.props.birth_year}</p>
-                <p>Hair color: {this.props.hair_color}</p>
               </div>
             </div>
           );
